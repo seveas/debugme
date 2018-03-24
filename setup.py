5e97f0079633d618c8394869745f6fc1e886ab3f
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2018-03-25 00:56:20 +0100

    Bump version after using this package for CI shenanigans

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name = "debugme",
-      version = "1.6",
+      version = "1.7",
       author = "Dennis Kaarsemaker",
       author_email = "dennis@kaarsemaker.net",
       license = "zlib",
